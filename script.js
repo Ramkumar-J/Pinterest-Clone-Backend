20c3d942f6777a2944ec2bbe4b9fa6e6478fd6f6
ramkumar <ramviru9@gmail.com>
2022-06-29 16:00:47 +0530

    commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const bcrypt=require("bcryptjs");
 const jwt=require("jsonwebtoken");
 const cors=require("cors");
 app.use(cors({
-    origin:"https://leafy-tiramisu-d395e5.netlify.app"
+    origin:["https://leafy-tiramisu-d395e5.netlify.app","http://localhost:3004"]
 }))
 const mongodb=require("mongodb");
 const mongoClient=mongodb.MongoClient;
